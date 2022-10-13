173cea269894c82f96e2225f5f4e229bc6b706ee
Ramin <farhadiramin91@gmail.com>
2022-10-13 17:08:01 -0400

    Fetch API
    API WAS added but needs to setWeatherData hook to get appropriate data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
+
 function App() {
+  const [weatherData, setWeatherData] = useState('');
+  useEffect(() => {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/forecast?lat=41.7107&lon=72.2081&appid=839dabeab893a48daf8fab18b8dd7b87'
+    )
+      .then((resp) => resp.json())
+      .then((res) => setWeatherData(res));
+  }, []);
+  console.log(weatherData);
   return (
     <div className="App">
       <Header />
