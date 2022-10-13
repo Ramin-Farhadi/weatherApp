9da52e154386dcf21993ff8833d4f11865fa20b2
Ramin <farhadiramin91@gmail.com>
2022-10-13 11:48:18 -0400

    Icon added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './css/search.css';
+import { FaSistrix } from 'react-icons/fa';
 function Search() {
   return (
     <div className="search">
       <input type="text" placeholder="Search City" />
+      <FaSistrix
+        style={{ position: 'absolute', color: 'white', right: 5, top: 7 }}
+      />
     </div>
   );
 }
